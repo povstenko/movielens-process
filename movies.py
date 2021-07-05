6d51ed922abc6cfca115302348062aee5775d599
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-07-05 15:31:17 +0300

    construct arguments

---
+++ movies.py
@@ -0,0 +1,43 @@
+
+
+# import the necessary packages
+import csv
+import argparse
+
+
+def construct_argument_parser() -> dict:
+    """Construct the argument parser and get the arguments
+
+    Returns
+    -------
+    dict
+        Dictionary of arguments and paramenters
+    """
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument("-n", "--topN", type=str,
+                    help="the number of top rated movies for each genre. (example: 3)")
+    ap.add_argument("-g", "--genres", type=str,
+                    help="user-defined genre filter. can be multiple. (example: Comedy|Adventure)")
+    ap.add_argument("-f", "--year_from", type=int,
+                    help="the lower boundary of year filter (example: 1980)")
+    ap.add_argument("-t", "--year_to", type=int,
+                    help="the lower boundary of year filter (example: 2010)")
+    ap.add_argument("-r", "--regexp", type=str,
+                    help="filter on name of the film (example: love)")
+
+    return vars(ap.parse_args())
+
+
+def main():
+    args = construct_argument_parser()
+
+    # check if user don't pass any arguments
+    if all(x == None for x in args.values()):
+        print('Pass arguments')
+
+    if args['topN']:
+        print('topN')
+
+
+if __name__ == "__main__":
+    main()
