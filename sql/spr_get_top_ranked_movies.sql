32de617bec2d435c7a14981a1051647760705b8c
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-07-16 23:51:29 +0300

    Update spr_get_top_ranked_movies.sql

--- sql/spr_get_top_ranked_movies.sql
+++ sql/spr_get_top_ranked_movies.sql
@@ -1,23 +1,3 @@
-DROP PROCEDURE IF EXISTS spr_split_str;
-CREATE PROCEDURE spr_split_str(
-    IN str NVARCHAR(255),
-    IN splitter NVARCHAR(255)
-)
-wholeblock:BEGIN
-    DECLARE n INT DEFAULT 0;
-    DECLARE i INT DEFAULT 0;
-    SET i = 1;
-    SET n = (CHAR_LENGTH(str) - CHAR_LENGTH(REPLACE(str, splitter, '')));
-    DROP TEMPORARY TABLE IF EXISTS tmp_splitted_str;
-    CREATE TEMPORARY TABLE tmp_splitted_str(str NVARCHAR(255));
-
-    WHILE i <= n+1 DO
-        INSERT INTO tmp_splitted_str
-        SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(str, splitter, i), splitter, -1);
-        SET i = i + 1;
-    END WHILE;
-END;
-
 DROP PROCEDURE IF EXISTS spr_find_top_rated_movies;
 CREATE PROCEDURE spr_find_top_rated_movies(
     IN n int,
