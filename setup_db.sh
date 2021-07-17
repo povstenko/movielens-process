f73c54e4ce7efd789c44f746059d5ca4c8bc2ce3
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-07-17 20:38:10 +0300

    Update setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -3,14 +3,21 @@
 #author:  Vitaliy Povstenko
 #date:    17/07/2021
 
-host="127.0.0.1"
-port="3307"
-user="client"
-pass="DCCcuons"
-db="movies_db"
+while getopts h:P:u:p:d: flag
+do
+    case "${flag}" in
+        h) host=${OPTARG};;
+        P) port=${OPTARG};;
+        u) user=${OPTARG};;
+        p) pass=${OPTARG};;
+        d) db=${OPTARG};;
+    esac
+done
 
 mysql -h $host --port=$port -u$user -p$pass < sql/movies_db.sql
 mysql -h $host --port=$port $db -u$user -p$pass < sql/movies_table.sql
 mysql -h $host --port=$port $db -u$user -p$pass < sql/ratings_table.sql
 mysql -h $host --port=$port $db -u$user -p$pass < sql/spr_split_str.sql
 mysql -h $host --port=$port $db -u$user -p$pass < sql/spr_get_top_ranked_movies.sql
+
+#END
